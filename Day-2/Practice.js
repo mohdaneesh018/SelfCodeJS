b788958cfe9403486f96590423e66604dfcdbd94
mohdaneesh018 <mdaneesh537@gmail.com>
2025-07-23 10:24:29 +0530

    Updates

--- Day-2/Practice.js
+++ Day-2/Practice.js
@@ -1,19 +1,47 @@
-// 1. Print numbers from 1 to 10:-
-for (i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// // 1. Print numbers from 1 to 10:-
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
 
 
 
 
-// 2. Print even numbers from 1 to 20:-
-for (i = 1; i <= 20; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-}
-// OR 
-// for (let i = 2; i <= 20; i += 2) {
+// // 2. Print even numbers from 1 to 20:-
+// for (i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+// // OR 
+// // for (let i = 2; i <= 20; i += 2) {
+// //     console.log(i);
+// // }
+
+
+
+
+
+// // 3. Find the sum of first 5 natural numbers:-
+// let sum = 0;
+// for (i = 1; i <= 5; i++) {
+//     sum += i;
+// }
+// console.log("Total sum is:", sum);
+
+
+
+
+// // 4. Print table of 7:-
+// let num = 7;
+// for (i = 1; i <= 10; i++) {
+//     console.log(i * 7);
+// }
+
+
+
+
+// 5. Print numbers from 10 to 1 (reverse):-
+// for (i = 10; i >= 1; i--) {
 //     console.log(i);
 // }
 
@@ -21,187 +49,159 @@ for (i = 1; i <= 20; i++) {
 
 
 
-// 3. Find the sum of first 5 natural numbers:-
-let sum = 0;
-for (i = 1; i <= 5; i++) {
-    sum += i;
-}
-console.log("Total sum is:", sum);
 
 
 
 
-// 4. Print table of 7:-
-let num = 7;
-for (i = 1; i <= 10; i++) {
-    console.log(i * 7);
-}
+// Using While Loop
 
+// 1. Print even numbers from 1 to 20 using while:-
+// let i = 1;
+// while (i <= 20) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
 
 
 
-5. Print numbers from 10 to 1 (reverse):-
-for (i = 10; i >= 1; i--) {
-    console.log(i);
-}
 
+// 2. Print numbers from 10 to 1 (reverse):-
+// let num = 10;
+// let i= 1;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
 
 
 
 
+// 3. Print the table of 9 using while:-
+// let nums = 9;
+// let j = 1;
+// while(j <= 10){
+//     console.log(`${j} * ${nums} = ${j * nums}`);
+//     j++;
+// }
 
 
 
 
-Using While Loop
+// 4. Find the sum of first 10 natural numbers using while:-
+// let sum = 0;
+// let x = 1;
+// while(x <= 10){
+//     sum += x;
+//     x++;
+// }
+// console.log("Total sum is:", sum);
 
-1. Print even numbers from 1 to 20 using while:-
-let i = 1;
-while (i <= 20) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-    i++;
-}
 
 
 
 
-2. Print numbers from 10 to 1 (reverse):-
-let num = 10;
-let i= 1;
-while(i >= 1){
-    console.log(i);
-    i--;
-}
+// 5. Print only odd numbers from 1 to 15:-
+// let i = 1;
+// while (i <= 15) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+//     i++;
+// }
 
 
 
 
-3. Print the table of 9 using while:-
-let nums = 9;
-let j = 1;
-while(j <= 10){
-    console.log(`${j} * ${nums} = ${j * nums}`);
-    j++;
-}
 
 
 
+// // Do-while Loop
 
-4. Find the sum of first 10 natural numbers using while:-
-let sum = 0;
-let x = 1;
-while(x <= 10){
-    sum += x;
-    x++;
-}
-console.log("Total sum is:", sum);
+// // 1. Print numbers from 1 to 10 using do...while:-
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10)
 
 
 
 
+// // 2. Print table of 6 using do...while:-
+// let i = 1;
+// let num = 6;
+// do {
+//     console.log(`${num} * ${i} = ${num * i}`);
+//     i++;
+// } while (i <= 10)
 
-5. Print only odd numbers from 1 to 15:-
-let i = 1;
-while (i <= 15) {
-    if (i % 2 == 1) {
-        console.log(i);
-    }
-    i++;
-}
 
 
 
 
 
+// // 3. Find the sum of first 7 natural numbers using do...while:-
+// let sum = 0;
+// let i = 1;
+// do {
+//     sum += i;
+//     i++;
+// } while (i <= 7)
+// console.log("Total sum is:", sum);
 
 
-// Do-while Loop
 
-// 1. Print numbers from 1 to 10 using do...while:-
-let i = 1;
-do {
-    console.log(i);
-    i++;
-} while (i <= 10)
 
 
+// // 4. Print only odd numbers from 1 to 15 using do...while:-
+// let i = 1;
+// do {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 15)
 
 
-// 2. Print table of 6 using do...while:-
-let i = 1;
-let num = 6;
-do {
-    console.log(`${num} * ${i} = ${num * i}`);
-    i++;
-} while (i <= 10)
 
 
 
+// // 5. Print numbers from 10 to 1 (reverse) using do...while:-
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// } while ( i >= 1)
 
 
 
-// 3. Find the sum of first 7 natural numbers using do...while:-
-let sum = 0;
-let i = 1;
-do {
-    sum += i;
-    i++;
-} while (i <= 7)
-console.log("Total sum is:", sum);
 
 
 
+// // 6. Print even numbers from 2 to 20:
+// let i = 2;
+// do {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 20);
 
 
-// 4. Print only odd numbers from 1 to 15 using do...while:-
-let i = 1;
-do {
-    if (i % 2 == 1) {
-        console.log(i);
-    }
-    i++;
-} while (i <= 15)
 
 
 
-
-
-// 5. Print numbers from 10 to 1 (reverse) using do...while:-
-let i = 10;
-do {
-    console.log(i);
-    i--;
-} while ( i >= 1)
-
-
-
-
-
-
-// 6. Print even numbers from 2 to 20:
-let i = 2;
-do {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-    i++;
-} while (i <= 20);
-
-
-
-
-
-// 7. Find sum of first 5 odd numbers (1+3+5+7+9):-
-let sum = 0;
-let i = 1;
-let count = 0;
-do {
-    if (i % 2 == 1) {
-        sum += i;
-        count++;
-    }
-    i++;
-} while (count < 5)
-console.log("Total sum is:", sum);
+// // 7. Find sum of first 5 odd numbers (1+3+5+7+9):-
+// let sum = 0;
+// let i = 1;
+// let count = 0;
+// do {
+//     if (i % 2 == 1) {
+//         sum += i;
+//         count++;
+//     }
+//     i++;
+// } while (count < 5)
+// console.log("Total sum is:", sum);
