5d4d10b4062a8b5f9a835f15fbaaf75336455f80
mohdaneesh018 <mdaneesh537@gmail.com>
2025-08-11 10:25:31 +0530

    Updates

---
+++ Day-9/Example.js
@@ -0,0 +1,41 @@
+// Split Method Syntax
+// string.split(separator, limit);
+
+
+
+
+// Qs.
+// let str = "apple, banana, grapes";
+// let newArr = str.split(",");
+// console.log(newArr);
+
+
+
+// Join Method Syntax 
+// array.join(separator);
+
+
+
+
+
+// Qs. 
+// let fruits = ["apple", "banana", "grapes"];
+// let newFruits = fruits.join(" - ");
+// console.log(newFruits);
+
+
+
+
+
+
+// Entries Method Syntax
+// array.entries();
+
+
+
+// Qs. 
+// let fruits = ["apple", "banana", "orange"];
+// let iterator = fruits.entries();
+// for (let [index, value] of iterator) {
+//     console.log(index, value);
+// } 
---
+++ Day-9/practice.js
@@ -0,0 +1,43 @@
+// Split Method
+
+Qs. "Hello World" ko space ke hisab se array me badlo.
+let a = "Hello World";
+let b = a.split(" ");
+console.log(b);
+
+
+
+
+
+Qs. "a-b-c-d" ko - ke basis pe tod ke array banao.
+let a = "a-b-c-d";
+let b = a.split("-");
+console.log(b);
+
+
+
+
+
+
+Qs. "Javascript is fun" ko har word alag karo.
+let i = "Javascript is fun";
+let j = i.split(" ");
+console.log(j); 
+
+
+
+
+Qs. "12345" ko har digit alag karo.
+let i = "12345";
+let j = i.split("");
+console.log(j);
+
+
+
+
+
+
+Qs. Ek string "apple orange banana" ko tod kar array banao, aur pehla word print karo.
+let i = "apple orange banana"; 
+let j = i.split(" ");
+console.log(j[0]); 
