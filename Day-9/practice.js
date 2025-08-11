e5fe9dfe137a131cfad327bdb5029d7496bf4ae3
mohdaneesh018 <mdaneesh537@gmail.com>
2025-08-11 10:47:40 +0530

    Updates

--- Day-9/practice.js
+++ Day-9/practice.js
@@ -1,4 +1,4 @@
-// Split Method
+//1. Split Method
 
 Qs. "Hello World" ko space ke hisab se array me badlo.
 let a = "Hello World";
@@ -41,3 +41,58 @@ Qs. Ek string "apple orange banana" ko tod kar array banao, aur pehla word print
 let i = "apple orange banana"; 
 let j = i.split(" ");
 console.log(j[0]); 
+
+
+
+
+
+
+
+
+// 2. Join Method
+
+Qs. ["red", "green", "blue"] ko ek string me comma ke saath join karo.
+let i = ["red", "green", "blue"];
+let j = i.join(", ");
+console.log(j);
+
+
+
+
+
+
+
+Qs. ["HTML", "CSS", "JS"] ko " | " separator ke saath join karo.
+let i = ["HTML", "CSS", "JS"];
+let j = i.join(" | ");
+console.log(j);
+
+
+
+
+
+
+Qs. [1, 2, 3, 4] ko space ke saath join karo.
+let i = [1, 2, 3, 4];
+let j = i.join(" ");
+console.log(j);
+
+
+
+
+
+
+Qs. ["apple", "mango"] ko bina separator join karo.
+let i = ["apple", "mango"];
+let j = i.join("");
+console.log(j);
+
+
+
+
+
+
+Qs. Split & Join ka use karke "my name is Aneesh" ko har word uppercase karke join karo.
+let i = "my name is Aneesh";
+let j = i.split(" ").map((word) => word.toUpperCase()).join(" "); 
+console.log(j);
