ee1017eae5d773699bc1b6e736e0c5f7fe38046a
mohdaneesh018 <mdaneesh537@gmail.com>
2025-08-13 11:53:36 +0530

    Updates

--- Day-11/Example.js
+++ Day-11/Example.js
@@ -1,47 +1,47 @@
-// Function 
+Function 
 
-// Qs. Addition of two variables without Parameter.
-// let a = 10;
-// let b = 20;
+Qs. Addition of two variables without Parameter.
+let a = 10;
+let b = 20;
 
-// function AddTwoVariable() {
-//     console.log(a + b);
-// }
-// AddTwoVariable();
-
-
-
-
-
-// Qs. Addition of two variables with Parameters.
-// let a = 10;
-// let b = 20;
-// function addVar(num1, num2) {
-//     console.log(num1 + num2);
-// }
-// addVar(a, b);
-// addVar(40, 50);
+function AddTwoVariable() {
+    console.log(a + b);
+}
+AddTwoVariable();
 
 
 
 
 
-
-// Qs. 
-// function numVariable(a, b) {
-//     console.log(a + b, "hi");
-//     return a + b;
-// }
-// numVariable(240, 100);
-// const output = numVariable(40, 50);
-// console.log(output, "output");
+Qs. Addition of two variables with Parameters.
+let a = 10;
+let b = 20;
+function addVar(num1, num2) {
+    console.log(num1 + num2);
+}
+addVar(a, b);
+addVar(40, 50);
 
 
 
 
 
 
-// Qs.
+Qs. 
+function numVariable(a, b) {
+    console.log(a + b, "hi");
+    return a + b;
+}
+numVariable(240, 100);
+const output = numVariable(40, 50);
+console.log(output, "output");
+
+
+
+
+
+
+Qs.
 let folders = ["Day-1", "Day-2", "Day-3", "Day-4", "Day-5"];
 let userTyped = "Day-4";
 
