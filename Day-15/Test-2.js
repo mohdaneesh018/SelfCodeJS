cd7b344e7c3e809ef94f0f0c6e5a20571adabaf9
mohdaneesh018 <mdaneesh537@gmail.com>
2025-08-19 12:48:02 +0530

    Updates

--- Day-15/Test-2.js
+++ Day-15/Test-2.js
@@ -1,117 +1,257 @@
-// 1. 1. Reverse an array
-// Reverse the elements of an array manually (without using .reverse()).
-// Input 1: [1, 2, 3]
-// Output 1: [3, 2, 1]
-// Input 2: [10, 20, 30]
-// Output 2: [30, 20, 10]
+1. 1. Reverse an array
+Reverse the elements of an array manually (without using .reverse()).
+Input 1: [1, 2, 3]
+Output 1: [3, 2, 1]
+Input 2: [10, 20, 30]
+Output 2: [30, 20, 10]
 
-// // let arr = [1, 2, 3]; 
-// let arr = [10, 20, 30];
-// let result = [];
+// let arr = [1, 2, 3]; 
+let arr = [10, 20, 30];
+let result = [];
 
-// function reverseArray(arr) {
-//     for (let i = arr.length - 1; i >= 0; i--) {
-//         result.push(arr[i])
-//     }
-//     return result;
-// }
-// console.log(reverseArray(arr));
-
-
+function reverseArray(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        result.push(arr[i])
+    }
+    return result;
+}
+console.log(reverseArray(arr));
 
 
 
-// 2. Find second largest number in an array
-// Return the second largest unique number from an array.
-// Input 1: [10, 5, 20, 8]
-// Output 1: 10
-// Input 2: [1, 2, 2, 3]
-// Output 2: 2
 
-// let arr = [10, 5, 20, 8];
+
+2. Find second largest number in an array
+Return the second largest unique number from an array.
+Input 1: [10, 5, 20, 8]
+Output 1: 10
+Input 2: [1, 2, 2, 3]
+Output 2: 2
+
+let arr = [10, 5, 20, 8];
+let arr = [1, 2, 2, 3];
+let max = 0;
+let secMax = 0;
+
+function secondLargest(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secMax = max;
+            max = arr[i];
+        } else if ((arr[i] > secMax) && (arr[i] > max)) {
+            secMax = arr[i];
+        }
+    }
+    return secMax;
+}
+console.log(secondLargest(arr));
+
+
+
+
+
+
+3. Remove duplicates from array
+Return a new array with all duplicates removed (no Set).
+Input 1: [1, 2, 2, 3]
+Output 1: [1, 2, 3]
+Input 2: [5, 5, 5, 1]
+Output 2: [5, 1]
+
 // let arr = [1, 2, 2, 3];
-// let max = 0;
-// let secMax = 0;
+let arr = [5, 5, 5, 1];
+let result = [];
 
-// function secondLargest(arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] > max) {
-//             secMax = max;
-//             max = arr[i];
-//         } else if ((arr[i] > secMax) && (arr[i] > max)) {
-//             secMax = arr[i];
-//         }
-//     }
-//     return secMax;
-// }
-// console.log(secondLargest(arr));
+function removeDuplicates(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if(!result.includes(arr[i])){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(removeDuplicates(arr));
 
 
 
 
 
-
-// 3. Remove duplicates from array
-// Return a new array with all duplicates removed (no Set).
-// Input 1: [1, 2, 2, 3]
-// Output 1: [1, 2, 3]
-// Input 2: [5, 5, 5, 1]
-// Output 2: [5, 1]
-
-// // let arr = [1, 2, 2, 3];
-// let arr = [5, 5, 5, 1];
-// let result = [];
-
-// function removeDuplicates(arr) {
-//     for(let i = 0; i < arr.length; i++){
-//         if(!result.includes(arr[i])){
-//             result.push(arr[i]);
-//         }
-//     }
-//     return result;
-// }
-// console.log(removeDuplicates(arr));
+4. Check if two arrays are equal
+Compare if two arrays have same elements in same order.
+Input 1: [1, 2, 3], [1, 2, 3]
+Output 1: true
+Input 2: [1, 2], [2, 1]
+Output 2: false
 
 
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
 
+let arr1 = [1, 2];
+let arr2 = [2, 1];
+let isCheck = false
 
-
-// 4. Check if two arrays are equal
-// Compare if two arrays have same elements in same order.
-// Input 1: [1, 2, 3], [1, 2, 3]
-// Output 1: true
-// Input 2: [1, 2], [2, 1]
-// Output 2: false
-
-
-// let arr1 = [1, 2, 3];
-// let arr2 = [1, 2, 3];
-
-// let arr1 = [1, 2];
-// let arr2 = [2, 1];
-// let isCheck = false
-
-// function arrayAreEqual(arr1, arr2) {
-//     for(let i = 0; i < arr1.length; i++){
-//         if(arr1[i] != arr2[i]){
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-// console.log(arrayAreEqual(arr1, arr2));
+function arrayAreEqual(arr1, arr2) {
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] != arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(arrayAreEqual(arr1, arr2));
 
 
 
 
 
 
-// 5. . Rotate array k times to the right
-// Rotate array elements k times to the right.
-// Input 1: [1, 2, 3, 4], k = 2
-// Output 1: [3, 4, 1, 2]
-// Input 2: [10, 20, 30], k = 1
-// Output 2: [30, 10, 20]
+5. . Rotate array k times to the right
+Rotate array elements k times to the right.
+Input 1: [1, 2, 3, 4], k = 2
+Output 1: [3, 4, 1, 2]
+Input 2: [10, 20, 30], k = 1
+Output 2: [30, 10, 20]
 
 // let arr = [1, 2, 3, 4];
 // let k = 2;
+let arr = [10, 20, 30];
+let k = 1;
+function rotateK(arr) {
+    for (let x = 0; x < k; x++) {
+        let lastElement = arr[arr.length - 1];
+        for (let i = arr.length - 1; i >= 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = lastElement;
+    }
+    return arr;
+}
+console.log(rotateK(arr));
+
+
+
+
+
+
+6. Count vowels in a string
+Count number of vowels (a, e, i, o, u) in a given string.
+Input 1: "hello"
+Output 1: 2
+Input 2: "JAVASCRIPT"
+Output 2: 3
+
+// let str = "hello";
+let str = "JAVASCRIPT";  
+let count = 0;
+let vowels = ["a", "e", "i", "o", "u"];
+function countVowels(str) {
+    let string = str.toLowerCase();
+    for (let i = 0; i <= str.length; i++) {
+        if (vowels.includes(string[i])){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels(str));
+
+
+
+
+
+
+7. Print triangle pattern using loops
+Write a loop to print * pattern based on number of rows.
+Input 1: 3
+Output 1: *
+          * *
+          * * *
+Input 2: 2
+Output 2: *
+          * *
+
+// let n = 3;
+let n = 5;
+function printPattern(n) {
+    let res = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+            res += "* ";
+        }
+        res += "\n";
+    }
+    return res;
+}
+console.log(printPattern(n));
+
+
+
+
+
+
+8. Factorial using recursion
+Calculate factorial of a number using recursion.
+Input 1: 5
+Output 1: 120
+Input 2: 3
+Output 2: 6
+
+function factorial(n) {
+    if(n === 0 || n === 1){
+        return 1;
+    } 
+    return n * factorial(n - 1);
+}
+console.log(factorial(5));
+console.log(factorial(3));
+
+OR
+
+function factorial(n) {
+    let result = 1;
+    for(let i = 2; i <= n; i++){           // i = 1 se  hi kr skte h 
+        result = result * i;
+    }
+    return result;
+}
+console.log(factorial(4));
+console.log(factorial(0));
+
+
+
+
+
+
+9. Check if number is prime
+Return true if number is prime (only divisible by 1 and itself).
+Input 1: 7
+Output 1: true
+Input 2: 9
+Output 2: false
+
+function numberPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(numberPrime(7));
+console.log(numberPrime(9)); 
+
+
+
+
+
+
+10. Find Fibonacci number at Nth position
+Return the Nth Fibonacci number (0-indexed).
+Input 1: 5
+Output 1: 5
+Input 2: 6
+Output 2: 8
