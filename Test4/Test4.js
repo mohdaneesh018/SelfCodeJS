4ca45e320e1ced3edb3ce8021bf9c9f3239ee3b4
mohdaneesh018 <mdaneesh537@gmail.com>
2025-09-05 12:11:28 +0530

    Updates

--- Test4/Test4.js
+++ Test4/Test4.js
@@ -103,5 +103,89 @@ function groupByRole(arr) {
 
     return result;
 }
-
 console.log(groupByRole(users));
+
+
+
+
+
+5. Filter Only Unique Binary Strings from Array:
+Input: ["101", "101", "000", "111", "000"]
+Output: ["101", "000", "111"]
+
+function uniqueBinary(arr) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(uniqueBinary(["101", "101", "000", "111", "000"]));
+
+
+
+
+
+
+6. Number Pyramid
+Input: 5
+Output:
+        1
+       121
+      12321
+     1234321
+    123454321
+
+let n = 5; 
+for (let i = 1; i <= n; i++) {
+    let res = "";
+    for (let spaces = 1; spaces <= (n - i); spaces++) {                // space k liye
+        res += " ";
+    } for (let num = 1; num <= i; num++) {                             // ascending num k liye
+        res += num;
+    } for (let x = i - 1; x >= 1; x--) {                               // descending num k liye
+        res += x;
+    }
+    console.log(res);
+}
+
+
+
+
+
+
+
+7. Diamond Pattern
+Input: 4
+Output:
+        *
+       ***
+      *****
+     *******
+      *****
+       ***
+        *
+
+let n = 4;
+for (let i = 1; i <= n; i++) {
+    let res = "";
+    for (let spaces = 1; spaces <= (n - i); spaces++) {
+        res += " ";
+    } for (let star = 1; star <= (2 * i) - 1; star++) {
+        res += "*";
+    }
+    console.log(res);
+}
+
+for (let i = 1; i <= n - 1; i++) {
+    let res = "";
+    for (let spaces = 1; spaces <= i; spaces++) {
+        res += " ";
+    } for (let star = 1; star <= 2 * (n - i) - 1; star++) {
+        res += "*";
+    }
+    console.log(res);
+} 
