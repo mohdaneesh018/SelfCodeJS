58d8c3842a3730b14c98ed2f1e080aec012b68c2
mohdaneesh018 <mdaneesh537@gmail.com>
2025-09-08 21:54:50 +0530

    Updates

---
+++ Test5/Test5.js
@@ -0,0 +1,34 @@
+✅ 2D Array Questions (5)
+1. Transpose of a Matrix
+Description: Convert rows to columns and vice versa.
+Input:
+[
+ [1, 2]
+ [3, 4],
+ [5, 6]
+]
+Output:
+[
+ [1, 3, 5],
+ [2, 4, 6]
+]
+
+let nums =
+    [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ]
+function transposeMatrix(nums) {
+    let result = [];
+
+    for (let i = 0; i < nums[0].length; i++) {
+        let row = [];
+        for (let j = 0; j < nums.length; j++) {
+            row.push(nums[j][i]);
+        }
+        result.push(row);
+    }
+    return result;
+}
+console.log(transposeMatrix(nums));
