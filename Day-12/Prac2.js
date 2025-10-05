347ca0a2d1f1e8928aa5cd608476fda4da28d2f3
mohdaneesh018 <mdaneesh537@gmail.com>
2025-10-05 14:07:51 +0530

    Updates

--- Day-12/Prac2.js
+++ Day-12/Prac2.js
@@ -1,6 +1,7 @@
-3️⃣ Objects 
+Objects
 
-1. Ek object laptop banao jisme brand, model, price ho. price change karo aur object print karo.
+1. Ek object laptop banao jisme brand, model, price ho.price change karo aur object print karo.
+
 const laptop = {
     model: "Lenovo",
     brand: "Graphics Card",
@@ -18,28 +19,30 @@ console.log(laptop);
 
 
 
-2. Ek object student banao jisme name, marks (array), aur ek method getAverage() ho jo average marks return kare.
+2. Ek object student banao jisme name, marks(array), aur ek method getAverage() ho jo average marks return kare.
+
 const student = {
     name: "Aneesh",
     marks: [20, 45, 32, 12],
 
-    getAverage: function() {
-        let sum = 0; 
-        for(let i = 0; i < this.marks.length; i++){
-        let average = sum / 3;
-            sum += this.marks[i]; 
+    getAverage: function () {
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i++) {
+            let average = sum / 3;
+            sum += this.marks[i];
         }
         return sum / this.marks.length;
     }
 };
-    console.log(student.getAverage());
+console.log(student.getAverage());
 
 
 
 
 
 
-3. Ek array of objects users banao jisme 3 log ho aur unke name aur age ho. Sirf unka naam print karo jinki age 18 se zyada hai.
+3. Ek array of objects users banao jisme 3 log ho aur unke name aur age ho.Sirf unka naam print karo jinki age 18 se zyada hai.
+
 const obj = [
     { name: "Rahul", age: 15 },
     { name: "Virat", age: 40 },
@@ -57,15 +60,16 @@ for (let i = 0; i < obj.length; i++) {
 
 
 
-4. Ek object shop banao jisme products ek array of objects ho (name, price). Ek method getTotal() banao jo total price return kare.
+4. Ek object shop banao jisme products ek array of objects ho(name, price).Ek method getTotal() banao jo total price return kare.
+
 const products = [
-    {name: "Bottle", price: 100},
-    {name: "Tiffin", price: 200},
-    {name: "Mixxer", price: 500}
+    { name: "Bottle", price: 100 },
+    { name: "Tiffin", price: 200 },
+    { name: "Mixxer", price: 500 }
 ]
 function getTotal() {
     let sum = 0;
-    for(let i = 0; i < products.length; i++) {
+    for (let i = 0; i < products.length; i++) {
         sum += products[i].price;
     }
     return sum;
@@ -81,23 +85,25 @@ console.log(getTotal());
 4️⃣ Loops with Arrays & Objects 
 
 5. Ek array colors ke saare elements for...of loop se print karo.
+
 let colors = ["red", "green", "blue", "yellow"];
 for (let element of colors) {
     console.log(element);
 }
-    
+
 
 
 
 
 
 6. Ek object person ke saare keys aur values for...in loop se print karo.
+
 let object = {
     name: "Rahul",
     age: 29,
     city: "Mumbai"
-} 
-for(let key in object){
+}
+for (let key in object) {
     console.log(key, ":", object[key]);
 }
 
@@ -107,16 +113,17 @@ for(let key in object){
 
 
 7. Ek array of numbers me se sirf even numbers print karo.
-let arr = [2, 4, 5, 7, 10];  
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] % 2 == 0){
+
+let arr = [2, 4, 5, 7, 10];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
         console.log(arr[i]);
     }
 }
 
 OR
 
-let arr = [2, 4, 5, 7, 10];  
+let arr = [2, 4, 5, 7, 10];
 for (let num of arr) {
     if (num % 2 === 0) {
         console.log(num);
@@ -127,14 +134,15 @@ for (let num of arr) {
 
 
 
-8. Ek array of objects employees banao jisme name aur salary ho. Sirf unka naam print karo jinki salary 50000 se zyada hai.
+8. Ek array of objects employees banao jisme name aur salary ho.Sirf unka naam print karo jinki salary 50000 se zyada hai.
+
 let obj = [
-    {name: "Saif", salary: 60000},
-    {name: "Sayyaf", salary: 70000},
-    {name: "Kaif", salary: 30000}
+    { name: "Saif", salary: 60000 },
+    { name: "Sayyaf", salary: 70000 },
+    { name: "Kaif", salary: 30000 }
 ]
-for(let i = 0; i < obj.length; i++){
-    if(obj[i].salary > 50000){
+for (let i = 0; i < obj.length; i++) {
+    if (obj[i].salary > 50000) {
         console.log(obj[i].name);
     }
 }
--- Day-12/Practice.js
+++ Day-12/Practice.js
@@ -1,80 +1,84 @@
-// Practice Questions
+Practice Questions
 
-// 1. Ek function banao sayHello jo ek naam lega parameter ke roop me aur "Hello <name>" print karega. Agar naam nahi diya gaya toh default "Guest" use ho.
-// function sayHello(name = "Guest") {
-//     console.log("Hello", name);
-// }
-// sayHello("Aneesh");                       // Hello Aneesh
-// sayHello();                               // Hello Guest
-
-
-
-
-
-
-
-// 2. Ek arrow function square banao jo ek number lega aur uska square return kare.
-// const square = (num) => {
-//     return num * num;
-// }
-// console.log(square(5));
-
-
-
-
-
-// 3. Ek function calculate banao jo 3 parameters lega: num1, num2, operator (string: "+", "-", "*", "/"). Operator ke hisaab se calculation kare aur result return kare.
-// function calculate(num1, num2, operator) { 
-//     if(operator == "+"){
-//         return num1 + num2;
-//     } else if(operator == "-"){
-//         return num1 - num2;
-//     } else if(operator == "*"){
-//         return num1 * num2;
-//     }else if(operator == "/"){
-//         return num1 / num2;
-//     } else {
-//         return "Invalid Operator";
-//     }
-// }
-// console.log(calculate(30, 15, "+"));
-// console.log(calculate(30, 15, "-"));
-// console.log(calculate(30, 15, "*"));
-// console.log(calculate(30, 15, "/"));
-// console.log(calculate(30, 15, "%"));
+1. Ek function banao sayHello jo ek naam lega parameter ke roop me aur "Hello <name>" print karega.Agar naam nahi diya gaya toh default "Guest" use ho.
+function sayHello(name = "Guest") {
+    console.log("Hello", name);
+}
+sayHello("Aneesh");                       // Hello Aneesh
+sayHello();                               // Hello Guest
 
 
 
 
 
 
-// 4. Ek arrow function isEven banao jo ek number lega aur return karega true agar even hai, warna false
-// const isEven = (n) => n % 2 == 0;
-// console.log(isEven(9));
 
-// OR
+2. Ek arrow function square banao jo ek number lega aur uska square return kare.
 
-// const isEven = (n) => {
-//     return n % 2 == 0;
-// }
-// console.log(isEven(10));
+const square = (num) => {
+    return num * num;
+}
+console.log(square(5));
 
 
 
 
 
+3. Ek function calculate banao jo 3 parameters lega: num1, num2, operator(string: "+", "-", "*", "/").Operator ke hisaab se calculation kare aur result return kare.
 
-// 5. Ek function countVowels banao jo ek string lega aur usme kitne vowels (a, e, i, o, u) hain wo return kare.
-// function countVowels(str) {
-//     let count = 0;
-//     let vowels = ["a", "e", "i", "o", "u"]
-//     str = str.toLowerCase();
-//     for (let i = 0; i < str.length; i++) {
-//         if (vowels.includes(str[i])) {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-// console.log(countVowels("apple")); 
-// console.log(countVowels("watermelon"));
+function calculate(num1, num2, operator) {
+    if (operator == "+") {
+        return num1 + num2;
+    } else if (operator == "-") {
+        return num1 - num2;
+    } else if (operator == "*") {
+        return num1 * num2;
+    } else if (operator == "/") {
+        return num1 / num2;
+    } else {
+        return "Invalid Operator";
+    }
+}
+console.log(calculate(30, 15, "+"));
+console.log(calculate(30, 15, "-"));
+console.log(calculate(30, 15, "*"));
+console.log(calculate(30, 15, "/"));
+console.log(calculate(30, 15, "%"));
+
+
+
+
+
+
+4. Ek arrow function isEven banao jo ek number lega aur return karega true agar even hai, warna false
+
+const isEven = (n) => n % 2 == 0;
+console.log(isEven(9));
+
+OR
+
+const isEven = (n) => {
+    return n % 2 == 0;
+}
+console.log(isEven(10));
+
+
+
+
+
+
+5. Ek function countVowels banao jo ek string lega aur usme kitne vowels(a, e, i, o, u) hain wo return kare.
+
+function countVowels(str) {
+    let count = 0;
+    let vowels = ["a", "e", "i", "o", "u"]
+    str = str.toLowerCase();
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("apple"));
+console.log(countVowels("watermelon"));
