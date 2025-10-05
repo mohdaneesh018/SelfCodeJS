020d334053644c5e0c14e953645115ad77f16cef
mohdaneesh018 <mdaneesh537@gmail.com>
2025-10-05 13:42:28 +0530

    Updates

--- Day-7/Example.js
+++ Day-7/Example.js
@@ -1,45 +1,29 @@
-// Array
+Array
 
-// Qs.
-// let num = [1, 2, 3];
-// let output = [];
-// for(let i = 0; i < num.length; i++){
-//     output.push(num[i] * 10);
-// }
-// console.log(output, "Output"); 
+Qs.
 
-
-
-
-
-
-// Qs.
-// let num = [12, 345, 32, 43, 67, 55, 56];
-// let count = 0;
-// for(let i = 0; i < num.length; i++){
-//     if(num[i] % 2 == 0){
-//         count++;
-//     }
-// }
-// console.log(count, "Count");
+let num = [1, 2, 3];
+let output = [];
+for (let i = 0; i < num.length; i++) {
+    output.push(num[i] * 10);
+}
+console.log(output, "Output");
 
 
 
 
 
 
+Qs.
 
-
-
-// Qs.
-// let arr = [12, 23, 45, 34, 56, 98, 78];
-// let target = 45;
-// for(let i = arr.length-1; i >= 0; i--){
-//     if(target == arr[i]){
-//         console.log(i);
-//          break;
-//     }
-// }
+let num = [12, 345, 32, 43, 67, 55, 56];
+let count = 0;
+for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 == 0) {
+        count++;
+    }
+}
+console.log(count, "Count");
 
 
 
@@ -48,27 +32,17 @@
 
 
 
-// Qs. using map method
-// let arr = ["virat", "rohit", "rahul"];
-// let newArr = arr.map((num) => num + " kohli");
-// console.log(newArr);
 
-// Qs.
-// let num = [1, 2, 3];
-// let newArr = num.map((num) => num * 10);
-// console.log(newArr);
+Qs.
 
-
-
-
-
-
-
-
-// Qs. using foreach
-// let arr = ["virat", "rohit", "rahul"];
-// let newArr = arr.forEach((student) => student + " kohli");
-// console.log(newArr);
+let arr = [12, 23, 45, 34, 56, 98, 78];
+let target = 45;
+for (let i = arr.length - 1; i >= 0; i--) {
+    if (target == arr[i]) {
+        console.log(i);
+        break;
+    }
+}
 
 
 
@@ -77,7 +51,39 @@
 
 
 
-// // Qs. using filter
-// let stu = [2, 3, 4, 5, 6, 9];
-// let newStu = stu.filter((i)=> i % 2 == 0);
-// console.log(newStu);
+Qs.using map method
+
+let arr = ["virat", "rohit", "rahul"];
+let newArr = arr.map((num) => num + " kohli");
+console.log(newArr);
+
+Qs.
+let num = [1, 2, 3];
+let newArr = num.map((num) => num * 10);
+console.log(newArr);
+
+
+
+
+
+
+
+
+Qs.using foreach
+
+let arr = ["virat", "rohit", "rahul"];
+let newArr = arr.forEach((student) => student + " kohli");
+console.log(newArr);
+
+
+
+
+
+
+
+
+Qs.using filter
+
+let stu = [2, 3, 4, 5, 6, 9];
+let newStu = stu.filter((i) => i % 2 == 0);
+console.log(newStu);
