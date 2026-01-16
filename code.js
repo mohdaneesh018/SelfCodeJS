7ce6800a94469b201e5c48797366ec42c4273ee7
mohdaneesh018 <mdaneesh537@gmail.com>
2026-01-16 19:35:18 +0530

    error solved

---
+++ code.js
@@ -0,0 +1,13 @@
+// let arr = [20, 40, 60, 13, 15];
+
+// for(let i = 0; i <= arr.length - 1; i++) { 
+//     if(arr[i] % 2 == 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+let x = 20;
+{
+    let x = 10;
+}
+console.log(x);
